cff442d1e8be9f16f72bf98c9c155f14f34fb35c
Dinuka Ashan <dinukaaw.sh@gmail.com>
2024-11-19 15:25:27 +0530

    Initial commit

--- database/store.sql
+++ database/store.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2020 at 05:52 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: Nov 19, 2024 at 10:30 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,25 +31,33 @@ CREATE TABLE `items` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `items`
 --
 
 INSERT INTO `items` (`id`, `name`, `price`) VALUES
-(1, 'Canon EOS', 36000),
+(1, 'Canon EOS', 40000),
 (2, 'Nikon DSLR', 40000),
 (3, 'Sony DSLR', 45000),
 (4, 'Olympus DSLR', 50000),
-(5, 'Titan Model #301', 13000),
-(6, 'Titan Model #201', 3000),
-(7, 'HMT Milan', 8000),
-(8, 'Faber Luba #111', 18000),
-(9, 'H&W', 800),
-(10, 'Luis Phil', 1000),
-(11, 'John Zok', 1500),
-(12, 'Jhalsani', 1300);
+(5, 'Wireless charger', 6500),
+(6, 'USB original charger', 3000),
+(7, 'Powerbank 20000w', 8000),
+(8, 'Single USB charger', 2200),
+(9, 'I phone 15 pro max', 420000),
+(10, 'Redmagic 9', 460000),
+(11, 'Samsung s24 ultra', 420000),
+(12, 'Google pixel 9', 480000),
+(14, 'Original JBL', 3500),
+(16, 'JBL headfone', 4200),
+(17, 'Box JBL', 3000),
+(18, 'Pocket JBL', 2000),
+(19, 'Sandisk 64GB pendrive', 3500),
+(20, 'Sandisk dual 64GB OTG pendrive', 5600),
+(21, 'Transcend 500GB External hard', 9000),
+(22, '1TB WD Elements hard drive', 12500);
 
 -- --------------------------------------------------------
 
@@ -66,19 +73,14 @@ CREATE TABLE `users` (
   `contact` varchar(255) NOT NULL,
   `city` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `contact`, `city`, `address`) VALUES
-(1, 'Vishwa', 'vish@gmail.com', '708175b3fdb269c4ebe8e7751bb3fccd', '9283746352', 'Bengaluru', 'JP Nagar 2nd Phase'),
-(2, 'Vishal', 'vishal@gmail.com', '72eea11febe23ae3dcab2bf59794c54c', '9120394750', 'Chennai', 'T Nagar'),
-(3, 'Ram', 'ram@gmail.com', '1b7b4c38f626766bbdcfc895e2c514f6', '9718273679', 'Delhi', 'PMO'),
-(4, 'Raj', 'raj@gmail.com', '1537c1dea8479ff52bc68336e323385f', '9281736475', 'Bengaluru', 'Basavangudi'),
-(5, 'Ravi', 'ravi@gmail.com', 'f4690de2554616f5b853d35f5a7970d4', '9182712098', 'Chennai', 'abc'),
-(6, 'Vishwadutt', 'vishwadutt99@gmail.com', '8231ac09972b51e692a5f515f8349a29', '9741301718', 'Bengaluru', 'Jayanagar 3rd Block');
+(10, 'Dinuka', 'dinuka.ashan@gmail.com', 'e36ff281c470b77c65b23e244b684c5d', '0123456789', 'colombo', 'colombo 3');
 
 -- --------------------------------------------------------
 
@@ -92,19 +94,17 @@ CREATE TABLE `user_item` (
   `item_id` int(11) NOT NULL,
   `status` enum('Added to cart','Confirmed','','') NOT NULL,
   `date_time` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user_item`
 --
 
 INSERT INTO `user_item` (`id`, `user_id`, `item_id`, `status`, `date_time`) VALUES
-(1, 1, 2, 'Confirmed', '2020-06-07 21:08:35'),
-(2, 1, 10, 'Confirmed', '2020-06-07 21:08:38'),
-(3, 3, 7, 'Confirmed', '2020-06-07 21:09:06'),
-(4, 3, 12, 'Confirmed', '2020-06-07 21:09:10'),
-(5, 5, 5, 'Confirmed', '2020-06-07 21:22:01'),
-(6, 5, 1, 'Confirmed', '2020-06-07 21:22:03');
+(38, 10, 5, 'Confirmed', '2024-11-19 14:56:46'),
+(39, 10, 17, 'Confirmed', '2024-11-19 14:57:13'),
+(40, 10, 8, 'Confirmed', '2024-11-19 14:57:16'),
+(41, 10, 6, 'Added to cart', '2024-11-19 14:57:47');
 
 --
 -- Indexes for dumped tables
@@ -136,19 +136,19 @@ ALTER TABLE `user_item`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user_item`
 --
 ALTER TABLE `user_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
